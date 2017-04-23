c102f1163c69385f4592521b91e62421ae736b52
Maria <s165266@student.dtu.dk>
2017-04-23 17:56:25 +0200

    kode til opgave2

--- kode/Kursusopg_3.m
+++ kode/Kursusopg_3.m
@@ -82,15 +82,62 @@ spectrogram(myRecording, kaiser(256,5), 220, 512, 8e3, 'yaxis')
 colormap jet
 title('Spectrogram');
 
-<<<<<<< HEAD
+
 %% Opgave 2b: 
+% kode fra Rabia
+recObj = audiorecorder(25000,16,2);
+get(recObj)
+
+recObj = audiorecorder;
+disp('Start speaking.')
+recordblocking(recObj,4);
+disp('End of recording.');
+
+play(recObj);
+myRecording3 = getaudiodata(recObj);
+
+figure(1)
+subplot(2,1,1),plot(myRecording);
+
+subplot(2,1,2)
+FFTKJER = fft(myRecording,25000);
+plot(abs(FFTKJER))
+
+fsample = 25000;
+N = 101;
+fcH = 950;
+fcL = 800;
+WcH = 2*pi*fcH/fsample;
+WcL = 2*pi*fcL/fsample;
+Wc = WcH-WcL;
+
+hn(0+(N+1))=Wc/pi;
+for index = 1:N;
+    hn(index+(N+1)) = sin(WcH*(index))/((index)*pi)-sin(WcL*(index))/((index*pi))
+end;
+for index = -N:-1;
+    hn(index+(N+1)) = sin(WcH*(-index))/((index)*pi)-sin(WcL*(-index))/((-index)*pi);
+end;
+hn
 
 
 
-=======
->>>>>>> 49c11e0db11585a90807cc4e4233afd4f3b1f0a7
 %% Opgave 3:
 
+figure()
+subplot(3,1,1)
+plot(Brumsignal);                   %Brumsignal = signal fra Kurt
+title('Tidsdom?ne'), ylabel('x(n)'), xlabel('n');
+
+subplot(3,1,2)
+FFTKJER = fft(Brumsignal,40000);    % Frekvensanalyse med fft().
+plot(abs(FFTKJER))
+title('Frekvensdom?ne'), ylabel('X(k)'), xlabel('k');
+
+subplot(3,1,3)                       % kurve over spectrogram.
+spectrogram(Brumsignal, kaiser(256,5), 220, 512, 8e3, 'yaxis')
+colormap jet
+title('Spectrogram');
 
 %% Opgave 4:
 
